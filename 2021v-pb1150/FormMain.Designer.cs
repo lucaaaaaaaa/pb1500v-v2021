94265470f6cf81c352d639a67a444c27b30e44c6
Luca Berg <luca@luca.moe>
2021-02-28 10:27:13 +0100

    Add tab control

--- 2021v-pb1150/FormMain.Designer.cs
+++ 2021v-pb1150/FormMain.Designer.cs
@@ -41,13 +41,18 @@ namespace _2021v_pb1150
             this.receivedDataTextBox = new System.Windows.Forms.TextBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(3, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 25);
             this.label1.TabIndex = 0;
@@ -56,7 +61,7 @@ namespace _2021v_pb1150
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Location = new System.Drawing.Point(3, 42);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 25);
             this.label2.TabIndex = 1;
@@ -65,7 +70,7 @@ namespace _2021v_pb1150
             // comPortSelect
             // 
             this.comPortSelect.FormattingEnabled = true;
-            this.comPortSelect.Location = new System.Drawing.Point(129, 6);
+            this.comPortSelect.Location = new System.Drawing.Point(114, 0);
             this.comPortSelect.Name = "comPortSelect";
             this.comPortSelect.Size = new System.Drawing.Size(158, 33);
             this.comPortSelect.TabIndex = 2;
@@ -79,14 +84,14 @@ namespace _2021v_pb1150
             "38400",
             "57600",
             "115200"});
-            this.bitRateSelect.Location = new System.Drawing.Point(129, 48);
+            this.bitRateSelect.Location = new System.Drawing.Point(114, 39);
             this.bitRateSelect.Name = "bitRateSelect";
             this.bitRateSelect.Size = new System.Drawing.Size(158, 33);
             this.bitRateSelect.TabIndex = 3;
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(129, 93);
+            this.connectButton.Location = new System.Drawing.Point(114, 78);
             this.connectButton.Name = "connectButton";
             this.connectButton.Size = new System.Drawing.Size(158, 45);
             this.connectButton.TabIndex = 4;
@@ -95,7 +100,7 @@ namespace _2021v_pb1150
             // 
             // disconnectButton
             // 
-            this.disconnectButton.Location = new System.Drawing.Point(129, 145);
+            this.disconnectButton.Location = new System.Drawing.Point(114, 129);
             this.disconnectButton.Name = "disconnectButton";
             this.disconnectButton.Size = new System.Drawing.Size(158, 43);
             this.disconnectButton.TabIndex = 5;
@@ -104,14 +109,14 @@ namespace _2021v_pb1150
             // 
             // sendDataTextBox
             // 
-            this.sendDataTextBox.Location = new System.Drawing.Point(437, 6);
+            this.sendDataTextBox.Location = new System.Drawing.Point(438, 3);
             this.sendDataTextBox.Name = "sendDataTextBox";
             this.sendDataTextBox.Size = new System.Drawing.Size(351, 31);
             this.sendDataTextBox.TabIndex = 6;
             // 
             // sendButton
             // 
-            this.sendButton.Location = new System.Drawing.Point(320, 6);
+            this.sendButton.Location = new System.Drawing.Point(321, 6);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(111, 45);
             this.sendButton.TabIndex = 7;
@@ -120,7 +125,7 @@ namespace _2021v_pb1150
             // 
             // receiveButton
             // 
-            this.receiveButton.Location = new System.Drawing.Point(320, 57);
+            this.receiveButton.Location = new System.Drawing.Point(321, 57);
             this.receiveButton.Name = "receiveButton";
             this.receiveButton.Size = new System.Drawing.Size(111, 45);
             this.receiveButton.TabIndex = 8;
@@ -129,11 +134,11 @@ namespace _2021v_pb1150
             // 
             // receivedDataTextBox
             // 
-            this.receivedDataTextBox.Location = new System.Drawing.Point(437, 57);
+            this.receivedDataTextBox.Location = new System.Drawing.Point(438, 40);
             this.receivedDataTextBox.Multiline = true;
             this.receivedDataTextBox.Name = "receivedDataTextBox";
             this.receivedDataTextBox.ReadOnly = true;
-            this.receivedDataTextBox.Size = new System.Drawing.Size(351, 381);
+            this.receivedDataTextBox.Size = new System.Drawing.Size(351, 324);
             this.receivedDataTextBox.TabIndex = 9;
             // 
             // statusStrip1
@@ -153,27 +158,62 @@ namespace _2021v_pb1150
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(240, 32);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 408);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.disconnectButton);
+            this.tabPage1.Controls.Add(this.connectButton);
+            this.tabPage1.Controls.Add(this.sendDataTextBox);
+            this.tabPage1.Controls.Add(this.receivedDataTextBox);
+            this.tabPage1.Controls.Add(this.bitRateSelect);
+            this.tabPage1.Controls.Add(this.sendButton);
+            this.tabPage1.Controls.Add(this.receiveButton);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comPortSelect);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 370);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 62);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.receivedDataTextBox);
-            this.Controls.Add(this.receiveButton);
-            this.Controls.Add(this.sendButton);
-            this.Controls.Add(this.sendDataTextBox);
-            this.Controls.Add(this.disconnectButton);
-            this.Controls.Add(this.connectButton);
-            this.Controls.Add(this.bitRateSelect);
-            this.Controls.Add(this.comPortSelect);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SoftSensConf";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,6 +233,9 @@ namespace _2021v_pb1150
         private System.Windows.Forms.TextBox receivedDataTextBox;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
